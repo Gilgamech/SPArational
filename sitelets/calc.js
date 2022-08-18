0ba84cad03ecbd6405670ec4e3f3f7a3255a4f34
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2022-08-17 16:05:26 -0800

    Add calc.js sitelet.

---
+++ sitelets/calc.js
@@ -0,0 +1,33 @@
+//Author: Stephen Gillie
+//Created: 8/17/2022
+//Updated: 8/17/2022
+//Notes: Add all 3 of the following lines to your index.html. The calculator should show up at the bottom of the page.
+//<script src="https://www.Sparational.com/Sparational.js"></script>
+//<script src='https://www.Sparational.com/sitelets/calc.js'></script>
+//<script>buildCalculatorSitelet('body');</script>
+
+function buildCalculatorSitelet($elementId) {
+	var calcInput = getBadPW()
+	addElement($elementId,"","","input","","","","","","onkeypress","detectEnter(event,function(){writeElement('"+calcInput+"',eval(readElement('"+calcInput+"')))})",calcInput)
+	var n = 1;
+	var calcTopRow = addElement($elementId);
+	addElement(calcTopRow,n,"","button","","","","appendElement('"+calcInput+"',"+n+")");n++
+	addElement(calcTopRow,n,"","button","","","","appendElement('"+calcInput+"',"+n+")");n++
+	addElement(calcTopRow,n,"","button","","","","appendElement('"+calcInput+"',"+n+")");n++
+	addElement(calcTopRow,"+","","button","","","","appendElement('"+calcInput+"','+')")
+	var calcMidRow = addElement($elementId);
+	addElement(calcMidRow,n,"","button","","","","appendElement('"+calcInput+"',"+n+")");n++
+	addElement(calcMidRow,n,"","button","","","","appendElement('"+calcInput+"',"+n+")");n++
+	addElement(calcMidRow,n,"","button","","","","appendElement('"+calcInput+"',"+n+")");n++
+	addElement(calcMidRow,"-","","button","","","","appendElement('"+calcInput+"','-')")
+	var calcBotRow = addElement($elementId);
+	addElement(calcBotRow,n,"","button","","","","appendElement('"+calcInput+"',"+n+")");n++
+	addElement(calcBotRow,n,"","button","","","","appendElement('"+calcInput+"',"+n+")");n++
+	addElement(calcBotRow,n,"","button","","","","appendElement('"+calcInput+"',"+n+")")
+	addElement(calcBotRow,"*","","button","","","","appendElement('"+calcInput+"','*')")
+	var calcZeroRow = addElement($elementId);
+	addElement(calcZeroRow,"0","","button","","","","appendElement('"+calcInput+"',0)");n++
+	addElement(calcZeroRow,".","","button","","","","appendElement('"+calcInput+"','.')")
+	addElement(calcZeroRow,"=","","button","","","","writeElement('"+calcInput+"',eval(readElement('"+calcInput+"')))")
+	addElement(calcZeroRow,"/","","button","","","","appendElement('"+calcInput+"','/')")
+}
