c9d8e677492e46c9b71d25951a9aa3784723460f
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2023-03-26 08:02:54 -0800

    //0.10.1: Revert scrollTable to a sitelet.

---
+++ sitelets/scrollingTable.js
@@ -0,0 +1,86 @@
+//Copyright 2013-2023 Gilgamech Technologies
+//scrollingTable.js v0.7.0
+//Author: Stephen Gillie
+//Created on: 3/26/2022
+//Last updated: 3/26/2023
+//Version history:
+//0.10.1: Revert scrollTable to a sitelet.
+
+function addTableRow(parentTableId,dataArray,beforeRow=0) {
+    const newNode = document.createElement("tr");
+    newNode.id = getBadPW();
+    const list = returnTablePart(parentTableId,'TBODY');
+    if (beforeRow == "end") {
+        beforeRow = list.children.length;
+    }
+    list.insertBefore(newNode, list.children[beforeRow]);
+    for (let n=0; n < dataArray.length; n++) {
+        addElement(newNode.id,dataArray[n],"","td")
+    }
+}
+
+function scrollTable(tableName,historyData,deBugVar="off") {
+//getElement(wrapperName).onscroll= function () {scrollTable(tableName)}
+    if (deBugVar=="debug") {console.log("scrollChart")};
+    let tableChildren = returnTablePart(tableName,'TBODY').children
+    let numberAbove = 0;
+    let numberBelow = 0;
+
+    for (let n=0; n < tableChildren.length; n++) {
+        let elementLocation = locateElement(tableChildren[n].id)[0] *1
+        if (elementLocation > 0) {
+            numberAbove++
+        } else if (elementLocation == 0) {
+            //In
+        } else if (elementLocation < 0) {
+            numberBelow++
+        } else {
+            console.log("Error - element "+tableChildren[n].id+" location is "+elementLocation)
+        }//end if elementLocation
+    }; //for let n
+
+    for (let n=0; n < tableChildren.length; n++) {
+        let elementLocation = locateElement(tableChildren[n].id)[0] *1
+        if (deBugVar=="debug") {console.log("elementLocation: "+elementLocation+" numberAbove: "+numberAbove+" numberBelow: "+numberBelow)};
+        
+        if (numberAbove > 2) {
+            //Delete any more than 2 above the window
+            let rowToChange = tableChildren[0];
+            deleteElement(rowToChange.id);//Delete top row
+            numberAbove--;
+            if (deBugVar=="debug") {console.log("Removing "+rowToChange.children[3].innerText+" - New numberAbove: "+numberAbove+" New numberBelow: "+numberBelow)};
+        } else if (numberAbove < 2) {
+            //Load another if only 1 above the window
+            let rowToChange = historyData[mdIndexOf(historyData,tableChildren[0].children[3].innerText)[0]-1]; //line above top
+            addTableRow(tableName,rowToChange)//Add to top
+            numberAbove++
+            if (deBugVar=="debug") {console.log("Adding "+rowToChange+" - New numberAbove: "+numberAbove+" New numberBelow: "+numberBelow)};
+        } else {
+            //This is expected, do nothing.
+        } 
+        if (numberBelow > 2) {
+            let rowToChange = tableChildren[tableChildren.length-1];
+            //Delete any more than 2 below the window
+            deleteElement(rowToChange.id);//Delete bottom row
+            numberBelow--
+            if (deBugVar=="debug") {console.log("Removing "+rowToChange.children[3].innerText+" - New numberAbove: "+numberAbove+" New numberBelow: "+numberBelow)};
+        } else if (numberBelow < 2) {
+            //Load another if only 1 below the window
+            let rowToChange = historyData[mdIndexOf(historyData,tableChildren[tableChildren.length-1].children[3].innerText)[0]+1]; //line below bottom
+            addTableRow(tableName,rowToChange,"end") //Add to bottom
+            numberBelow++
+            if (deBugVar=="debug") {console.log("Adding "+rowToChange+" - New numberAbove: "+numberAbove+" New numberBelow: "+numberBelow)};
+        } else {
+            //This is expected, do nothing.
+        } 
+    }; //for let n
+}//end scrollChart
+
+function buildScrollingTable(parentElement,tableName,arrayData,offset,style) {
+    if (!tableName) {
+        tableName = getBadPW();
+    }; 
+    wrapperName = getElement(parentElement).parentElement.id;
+    mdArrayToTable(addElement(parentElement,"",style),tableName,arrayData.slice(0,getElement(wrapperName).offsetHeight/offset));
+    getElement(wrapperName).onscroll= function () {scrollTable(tableName,arrayData)}
+}
