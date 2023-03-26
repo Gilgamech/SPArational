a0d62b94c951e7807b18f10bc4f891ff4304e237
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2023-03-26 08:35:22 -0800

    //0.12: Parameterize indexColumn.

--- sitelets/scrollingTable.js
+++ sitelets/scrollingTable.js
@@ -1,11 +1,12 @@
 //Copyright 2013-2023 Gilgamech Technologies
-//scrollingTable.js v0.7.0
+//scrollingTable.js v0.12
 //Author: Stephen Gillie
 //Created on: 3/26/2022
 //Last updated: 3/26/2023
 //Version history:
 //0.10.1: Revert scrollTable to a sitelet.
 //0.11: Rename historyData parameter of scrollTable function to arrayVar.
+//0.12: Parameterize indexColumn.
 
 function addTableRow(parentTableId,dataArray,beforeRow=0) {
     const newNode = document.createElement("tr");
@@ -20,7 +21,7 @@ function addTableRow(parentTableId,dataArray,beforeRow=0) {
     }
 }
 
-function scrollTable(tableName,arrayVar,deBugVar="off") {
+function scrollTable(tableName,arrayVar,indexColumn,deBugVar="off") {
 //getElement(wrapperName).onscroll= function () {scrollTable(tableName)}
     if (deBugVar=="debug") {console.log("scrollChart")};
     let tableChildren = returnTablePart(tableName,'TBODY').children
@@ -39,7 +40,7 @@ function scrollTable(tableName,arrayVar,deBugVar="off") {
             console.log("Error - element "+tableChildren[n].id+" location is "+elementLocation)
         }//end if elementLocation
     }; //for let n
-
+	
     for (let n=0; n < tableChildren.length; n++) {
         let elementLocation = locateElement(tableChildren[n].id)[0] *1
         if (deBugVar=="debug") {console.log("elementLocation: "+elementLocation+" numberAbove: "+numberAbove+" numberBelow: "+numberBelow)};
@@ -49,7 +50,7 @@ function scrollTable(tableName,arrayVar,deBugVar="off") {
             let rowToChange = tableChildren[0];
             deleteElement(rowToChange.id);//Delete top row
             numberAbove--;
-            if (deBugVar=="debug") {console.log("Removing "+rowToChange.children[3].innerText+" - New numberAbove: "+numberAbove+" New numberBelow: "+numberBelow)};
+            if (deBugVar=="debug") {console.log("Removing "+rowToChange.children[indexColumn].innerText+" - New numberAbove: "+numberAbove+" New numberBelow: "+numberBelow)};
         } else if (numberAbove < 2) {
             //Load another if only 1 above the window
             addTableRow(tableName,rowToChange)//Add to top
@@ -64,7 +65,7 @@ function scrollTable(tableName,arrayVar,deBugVar="off") {
             //Delete any more than 2 below the window
             deleteElement(rowToChange.id);//Delete bottom row
             numberBelow--
-            if (deBugVar=="debug") {console.log("Removing "+rowToChange.children[3].innerText+" - New numberAbove: "+numberAbove+" New numberBelow: "+numberBelow)};
+            if (deBugVar=="debug") {console.log("Removing "+rowToChange.children[indexColumn].innerText+" - New numberAbove: "+numberAbove+" New numberBelow: "+numberBelow)};
         } else if (numberBelow < 2) {
             //Load another if only 1 below the window
             let rowToChange = arrayVar[mdIndexOf(arrayVar,tableChildren[tableChildren.length-1].children[indexColumn].innerText)[0]+1]; //line below bottom
@@ -83,5 +84,5 @@ function buildScrollingTable(parentElement,tableName,arrayData,offset,style) {
     }; 
     wrapperName = getElement(parentElement).parentElement.id;
     mdArrayToTable(addElement(parentElement,"",style),tableName,arrayData.slice(0,getElement(wrapperName).offsetHeight/offset));
-    getElement(wrapperName).onscroll= function () {scrollTable(tableName,arrayData)}
+    getElement(wrapperName).onscroll= function () {scrollTable(tableName,arrayData,3,"debug")}
 }
