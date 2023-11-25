3d5549906afe7c0c2a4600a81ea1ccb0f748fe8a
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2023-11-25 12:10:51 -0800

    Stretch for slightly more bug coverage.

--- Sparational.js
+++ Sparational.js
@@ -385,7 +385,7 @@ function convertMdToSpa(markdown) {
 						break;
 				//This is where Bold gets droped - because it's unhandled. 
 					case "*":
-						out += "{\"elementParent\": \""+elementParent+"\",\"elementType\":\"b\",\"innerText\": \""+line.replace("**","")+"\"},"
+						out += "{\"elementParent\": \""+elementParent+"\",\"elementType\":\"b\",\"innerText\": \""+line.replaceAll("**","")+"\"},"
 						break;
 					default:
 						break;
