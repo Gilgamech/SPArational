30332232823bfb39718dbde9bcbae50770797f0e
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2023-12-13 15:56:08 -0800

    Remove parentElement from main HTTP Passthrough system.

--- Sparational-4Beta.js
+++ Sparational-4Beta.js
@@ -221,7 +221,7 @@ function rewriteJson(data,baseData) {
 			if (data[element].substr(0,2) == "$_") {//Replace-o-rama! Full SPA pages - now in JML or YAML - can support $_ "dollarsign-underscore" variable replacement from anywhere within the document.
 				data[element] = eval(data[element].replace("$_","baseData"))
 			} else if (data[element].substr(0,4) == "http") {//Drop your load in the road! Leave a URL at the start of any line to have the page eventually load and display that data.
-				data[element] = {"elementParent":"parentElement","elementType":"script","innerText":("convertWebElement(\"parentElement\",\""+data[element]+"\")")}
+				data[element] = {"elementType":"script","innerText":("convertWebElement(\"parentElement\",\""+data[element]+"\")")}
 			}; // end if element.substr
 		} else if (typeof data[element] === "object") { 
 			data[element] = rewriteJson(data[element],baseData)
