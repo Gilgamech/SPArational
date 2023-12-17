5d9e3352354de4253e4fea66689705a9cfbde983
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2023-12-17 12:05:45 -0800

    Revert null innerText check.

--- Sparational.js
+++ Sparational.js
@@ -416,7 +416,7 @@ function convertMdToJml(markdown,nestedParent = "parentElement") {
 			
 			let elementClass = topLine.replace(divRegex,"").replace(" "+elementHash,"")
 			let innerText = blockSplit.slice(1,blockSplit.length -1)[0]
-			if (!(innerText)){innerText="\"\""}
+			//if (!(innerText)){innerText="\"\""}
 
 			if (botLine.match(/^\{/)){
 				let action = "onClick"
