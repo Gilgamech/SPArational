8db5e50a985e973e9c0ab44d2d2b34c27d112c6e
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2024-01-21 10:39:08 -0800

    Additional bugfix.

--- Sparational.js
+++ Sparational.js
@@ -561,7 +561,7 @@ function convertMdToJml(markdown,nestedParent = "parentElement") {
 		} else if (block.substr(0,4).match(/[ ]{4}/g) || block.substr(0,3).match(/[```]{3}/g) || block.substr(0,3).match(/[~]{3}/g)) {//Code block - don't process anything.
 			let outerClass = ""
 			let innerClass = ""
-			out += "{\"elementType\":\"pre\",\"elementClass\":\""+outerClass+"\",\""+id+"\"},"
+			out += "{\"elementType\":\"pre\",\"elementClass\":\""+outerClass+"\",\"id\": \""+id+"\"},"
 			out += "{\"elementParent\": \""+id+"\",\"elementType\":\"code\",\"elementClass\":\""+innerClass+"\",\"innerText\":"+block.replace(/^[ ]{4}/g,"").replace(/\n[ ]{4}/g,"\n")+"\"},"
 			
 		} else if (block.substr(0,5).match(/^-[ ]\[[X ]\]/g)) {//Task List block - Nesting.
